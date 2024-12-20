d031ff7e338bb438c65be039dc6421bd995531b7
sriv95 <srivichai95@gmail.com>
2024-12-20 15:56:09 +0700

    lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,17 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int main(){
+    string i;
+    cout << "Press Enter 3 times to reveal your future.";
+    string grades[] = {"A","B+","B","C+","C","D+","D","F","W"};
+    getline(cin,i);
+    getline(cin,i);
+    getline(cin,i);
+    srand(time(0));
+    i = grades[rand()%9];
+    cout << "You will get "<< i <<" in this 261102.";
+    return 0;
+}
